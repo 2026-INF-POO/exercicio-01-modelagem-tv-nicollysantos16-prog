3a5d1f32acb8d9fcb76f4c36e15ae4edc4c094f7
nicollysantos16-prog <20251TI0095@ifba.edu.br>
2026-03-04 14:35:52 -0300

    Update TV.java
    Nicolly Samilly Santana Santos- 2B

--- TV.java
+++ TV.java
@@ -1,1 +1,33 @@
+public class TV {
+    int polegada;
+    int volume = 5;
+    String marca;
+    int voltagem;
+    int canal;
 
+    void ligar() {
+        int consumo = voltagem * polegada;
+        System.out.println("A TV ligou! O consumo é de: " + consumo + " Watts");
+    }
+
+    void desligar() {
+        System.out.println("A TV desligou!");
+    }
+
+    void aumentar_volume() {
+        if (volume < 10) {
+            volume++;
+            System.out.println("O volume é: " + volume);
+        } else {
+            System.out.println("Volume máximo atingido!");
+        }
+    }
+
+    void diminuir_volume() {
+        if (volume > 1) {
+            volume--;
+            System.out.println("Volume: " + volume);
+        } else {
+            System.out.println("Volume mínimo atingido!");
+        }
+    }
