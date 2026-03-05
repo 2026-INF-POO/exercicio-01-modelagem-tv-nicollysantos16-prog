431bea506e00f073bc0112370bb9ddfdd1aaadbf
nicollysantos16-prog <20251TI0095@ifba.edu.br>
2026-03-05 07:33:45 -0300

    Update TV.java
    Nicolly Samilly-2B

--- TV.java
+++ TV.java
@@ -1,4 +1,4 @@
-public class TV {
+public class TV { 
     int polegada;
     int volume = 5;
     String marca;
@@ -7,11 +7,11 @@ public class TV {
 
     void ligar() {
         int consumo = voltagem * polegada;
-        System.out.println("A TV ligou! O consumo é de: " + consumo + " Watts");
+        System.out.println("A TV ligada! O consumo é de: " + consumo + " Watts");
     }
 
     void desligar() {
-        System.out.println("A TV desligou!");
+        System.out.println("Uma TV desligou!");
     }
 
     void aumentar_volume() {
@@ -30,4 +30,28 @@ public class TV {
         } else {
             System.out.println("Volume mínimo atingido!");
         }
+    }
+
+    void subir_canal() {
+        canal++;
+        System.out.println("Canal atual: " + canal);
+    }
+
+    void descer_canal() {
+        if (canal > 1) {
+            canal--;
+            System.out.println("Canal atual: " + canal);
+        }
+    }
+
+    public static void main(String[] args) {
+        TV minhaTV = new TV();         
+        minhaTV.polegada = 50;
+        minhaTV.voltagem = 220;
+        minhaTV.canal = 10;
+        
+        minhaTV.ligar();
+        minhaTV.aumentar_volume();
+        minhaTV.subir_canal();
+        minhaTV.desligar();
     }
